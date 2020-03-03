456e4fc45e91bbed84756546d23faad30bd35a0e
Sourav Kabiraj <sourav.kabiraj@outlook.com>
2020-03-03 22:25:29 +0530

    #L30 | Sourav | Added multiple solution. [DP]

--- dynamic-programming/word-break.ts
+++ dynamic-programming/word-break.ts
@@ -1,7 +1,15 @@
+const dp = {}
+
 function isSentance(text: string, dictionary: string[]): boolean {
+    if (dp[text]) {
+        return dp[text];
+    }
+    console.log(text);
     if (dictionary.indexOf(text) >= 0) {
+        dp[text] = true;
         return true;
     } else if (text.length == 1) {
+        dp[text] = false;
         return false;
     } else {
         for (let i = 1; i < text.length - 1; i++) {
@@ -9,9 +17,11 @@ function isSentance(text: string, dictionary: string[]): boolean {
             const prolog = _text.slice(0, i);
             const postlog = _text.slice(i, _text.length);
             if (isSentance(prolog, dictionary) && isSentance(postlog, dictionary)) {
+                dp[text] = true;
                 return true;
             }
         }
+        dp[text] = false;
         return false;
     }
 }
